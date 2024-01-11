3afe1d6a5a5d3a8eb9da79e3c9d3cd61cefa0dfa
Cezary Malinowski <violex.cmalinowski@gmail.com>
2024-01-11 19:44:57 +0100

    add class Active for clickedElement

--- js/script.js
+++ js/script.js
@@ -6,15 +6,29 @@
 //   });
 
 const titleClickHandler = function(event){
-    console.log(event);
+    const clickedElement = this;
+    console.log('clickedElement (with plus): ' + clickedElement);
+
+    /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
   
-    /* remove class 'active' from all article links  */
-  
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+       
+        clickedElement.classList.add('active');
+    /* [DONE] remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts article.active');
+    
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }    
   
     /* get 'href' attribute from the clicked link */
+    
   
     /* find the correct article using the selector (value of 'href' attribute) */
   
